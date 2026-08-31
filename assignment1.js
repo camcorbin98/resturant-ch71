628112fcf7cd260e484c8cf12ec0223cc5d0ae1f
Cameron Corbin <camcorbin98@gmail.com>
2026-08-31 15:00:34 -0700

    describe what you changed

---
+++ assignment1.js
@@ -0,0 +1,27 @@
+const restaurantName = "La Cantina";
+const cuisine = "Mexican";
+const city = "San Diego";
+
+let todaysSpecial = "Tacos al pastor";
+let availableTables = 5;
+let status = "Open";
+
+console.log("Restaurant Report");
+console.log("Restaurant:" + restaurantName);
+console.log("Cuisine:" + cuisine);
+console.log("City:" + city);
+console.log("Today's special:" + todaysSpecial);
+console.log("Available tables:" + availableTables);
+console.log("Status:" + status);
+
+todaysSpecial = "Enchiladas verdes"
+availableTables = 2;
+
+console.log("");
+console.log("Updated Report");
+console.log("Restaurant:" + restaurantName);
+console.log("Cuisine:" + cuisine);
+console.log("City:" + city);
+console.log("Today's special:" + todaysSpecial);
+console.log("Available tables:" + availableTables)
+console.log("Status:" + status);
